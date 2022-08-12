4cf86016cae7de14fd769c654e020277155a645b
Jaeyong Park Desktop <kkomzi@gmail.com>
2022-08-12 16:54:11 +0900

    - update sample code

--- AppStateService/BlazorApp/Program.cs
+++ AppStateService/BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp;
+using BlazorApp.Services;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,5 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSingleton<AppState>();
 
 await builder.Build().RunAsync();
---
+++ AppStateService/BlazorApp/Services/AppState.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorApp.Services
+{
+    public class AppState
+    {
+        public AppState()
+        {
+        }
+        public bool IsLogin { get; set; }
+        public async Task UpdateLoginStatus()
+        {
+            if (OnUpdateLoginStatus != null)
+            {
+                await OnUpdateLoginStatus.Invoke();
+            }
+        }
+        public event Func<Task>? OnUpdateLoginStatus;
+    }
+}
