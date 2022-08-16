74e54f8efea71e30b6e1b3237096f4c989529fa5
Jaeyong Park Desktop <kkomzi@gmail.com>
2022-08-16 13:00:39 +0900

    - add bootstrap bootswatch theme

--- BlazorBootswatchTheme/BlazorApp/Program.cs
+++ BlazorBootswatchTheme/BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp;
+using BlazorApp.Services;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -6,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped<IThemeService, ThemeService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ BlazorBootswatchTheme/BlazorApp/Services/ThemeService.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorApp.Services
+{
+    public interface IThemeService
+    {
+        public const string DefaultTheme = "Default";
+        public static readonly string[] SupportedThemeList = new[]
+        {
+            DefaultTheme,
+            "Cerulean",
+            "Cosmo",
+            "Cyborg",
+            "Darkly",
+            "Flatly",
+        };
+        string CurrentTheme { get; }
+        Task ToggleTheme(string themeName);
+    }
+    public class ThemeService : IThemeService
+    {
+        public string CurrentTheme { get; private set; } = IThemeService.DefaultTheme;
+
+        public async Task ToggleTheme(string themeName)
+        {
+            CurrentTheme = themeName;
+            await Task.CompletedTask;
+        }
+    }
+}
